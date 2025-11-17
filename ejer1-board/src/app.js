da249bfafd12ee5da8f4fe4b36c91b34e8cd6e6b
rimafd <afoud.r@gmail.com>
2025-11-17 02:04:13 +0100

    partials(header+footer)with Mustache

--- ejer1-board/src/app.js
+++ ejer1-board/src/app.js
@@ -5,6 +5,12 @@ import bodyParser from 'body-parser';
 import router from './router.js';
 import './load_data.js';
 
+import fs from 'fs';
+import path from 'path';
+
+const dataPath = path.resolve('./data/data.json');
+const posts = JSON.parse(fs.readFileSync(dataPath, 'utf-8'));
+
 const app = express();
 
 app.use(express.static('./public'));
@@ -15,6 +21,35 @@ app.set('views', './views');
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use('/', router);
+console.log(posts);
 
+app.get('/', (req, res) => {
+  res.render('SELLORA', {
+    // Header 
+    title: "Welcome to SellOra",
+    subtitle: "Your Favorite Retro Shop",
+    tagline: "Vintage Consoles, Games & More",
+    backgroundImage: "/img/HEADER BACKGROUND.jpg",
+    homeLink: "SELLORA.html",
+    logoSrc: "/img/logo.png",
+    navbarText: "- SellOra",
+    navItems: [
+      { label: "Home", link: "#", active: "active" },
+      { label: "Shop", link: "/shop", active: "" },
+      { label: "About", link: "/about", active: "" },
+    ],
+
+    posts,
+
+// Footer
+    year: 2025,
+    brand: "SellOra",
+    instagram: "https://www.instagram.com/t1lol/",
+    twitter: "https://x.com/hideo_kojima_en",
+    followUsText: "Follow us on:"
+
+  });
+});
+
+app.use('/', router);
 app.listen(3000, () => console.log('Web ready in http://localhost:3000/'));
