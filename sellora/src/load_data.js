a0c8f440e5bd436a3629b587862a1a0f4ed02f02
NachoRonc <i.roncero.2024@alumnos.urjc.es>
2025-11-19 00:52:27 +0100

    demo reviews

--- sellora/src/load_data.js
+++ sellora/src/load_data.js
@@ -1,22 +1,35 @@
 import fs from 'node:fs/promises';
+import { ObjectId } from 'mongodb';
 import * as store from './store.js';
 
 const UPLOADS_FOLDER = './uploads';
 const DATA_FOLDER = './data';
+const dataFile = 'data.json';
 
-let dataFile = 'data.json';
+const dataString = await fs.readFile(`${DATA_FOLDER}/${dataFile}`, 'utf8');
+let products = JSON.parse(dataString);
 
-const dataString = await fs.readFile(DATA_FOLDER + '/' + dataFile, 'utf8');
-
-const products = JSON.parse(dataString);
+products = products.map(product => {
+    if (!Array.isArray(product.reviews)) {
+        product.reviews = [];
+    }
+    product.reviews = product.reviews.map(review => ({
+        _id: new ObjectId(),
+        author: review.author,
+        text: review.text,
+        rating: review.rating
+    }));
+    return product;
+});
 
 await store.deleteProducts();
-for(let product of products){
+
+for (const product of products) {
     await store.addProduct(product);
 }
 
 await fs.rm(UPLOADS_FOLDER, { recursive: true, force: true });
 await fs.mkdir(UPLOADS_FOLDER);
-await fs.cp(DATA_FOLDER + '/images', UPLOADS_FOLDER, { recursive: true });
+await fs.cp(`${DATA_FOLDER}/images`, UPLOADS_FOLDER, { recursive: true });
 
-console.log('Demo data loaded');
+console.log('Demo data loaded with reviews');
